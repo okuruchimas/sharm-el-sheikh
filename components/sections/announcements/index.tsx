a546e020c1a760583bc5091cad176deee44b6ce0
shulika <illia.shulika@gmail.com>
2024-06-25 22:02:51 +0300

    make recent announcement section with slice array

--- components/sections/announcements/index.tsx
+++ components/sections/announcements/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React from "react";
 import styled from "@emotion/styled";
 import { Title } from "../../layout/title";
 import AnnounceCard from "./children/announce-card";
