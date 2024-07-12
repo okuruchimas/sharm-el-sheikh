98c132bda1b15684a2355e2b3ecf111c49cca08c
YaroslavBoldyshev <yaroslavboldushev@gmail.com>
2024-07-12 16:19:17 +0200

    feat: image component

---
+++ components/layout/Image/index.tsx
@@ -0,0 +1,48 @@
+import { memo, type FC } from "react";
+import Image, { type ImageProps } from "next/image";
+// utils
+import styled from "@emotion/styled";
+
+interface NextImageProps extends ImageProps {
+  width: string;
+  height: string;
+  mWidth?: string;
+  mHeight?: string;
+}
+
+interface WrapperProps
+  extends Pick<NextImageProps, "width" | "height" | "mWidth" | "mHeight"> {}
+
+const NextImage: FC<NextImageProps> = ({
+  alt = "",
+  width,
+  height,
+  mWidth,
+  mHeight,
+  ...props
+}) => (
+  <LogoWrapper
+    width={width}
+    height={height}
+    mWidth={mWidth ?? width}
+    mHeight={mHeight ?? height}
+  >
+    <Image {...props} alt={alt} layout="fill" />
+  </LogoWrapper>
+);
+
+const LogoWrapper = styled("div", {
+  shouldForwardProp: (prop) =>
+    !["mHeight", "mWidth", "width", "height"].includes(prop),
+})<WrapperProps>(({ theme, width, height, mHeight, mWidth }) => ({
+  position: "relative",
+  width,
+  height,
+
+  [theme.breakpoints.mobile]: {
+    width: mWidth,
+    height: mHeight,
+  },
+}));
+
+export default memo(NextImage);
