af8494132657514867eeb69d17c2302bc3243cb3
okuruchimas <lianeso4kas@gmail.com>
2025-05-26 14:14:24 +0200

    added full addvertisement modal window, updated styles

---
+++ components/sections/agents/delivery/children/full-add.tsx
@@ -0,0 +1,166 @@
+import Modal from "../../../../layout/modal";
+import TextAndIcon from "../../../../layout/text-and-icon";
+import Image from "next/image";
+import styled from "@emotion/styled";
+import { Title } from "../../../../layout/title";
+import { AdvertisementFragment } from "../../../../../gql/graphql";
+import { SellerInfo } from "./seller-info";
+import { FlexContainer } from "./short-add";
+import Button from "../../../../layout/button";
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  location: string;
+  price: string;
+  date: string;
+  imageUrl: string;
+  imageAlt?: string;
+  otherAddInfo: OtherAdInfo;
+}
+type OtherAdInfo = Omit<AdvertisementFragment, "title" | "price" | "location">;
+const FullAdd = ({
+  title,
+  location,
+  price,
+  date,
+  imageUrl,
+  imageAlt,
+  isOpen,
+  onClose,
+  otherAddInfo,
+}: Props) => {
+  const {
+    description,
+    contactMethod,
+    sellerName,
+    mobile,
+    email,
+    personalCardLink,
+  } = otherAddInfo;
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <Content>
+        <TopWrap>
+          <ImgWrapper>
+            <Image src={imageUrl} alt={imageAlt} layout="fill" />
+          </ImgWrapper>
+          <InfoWrap>
+            <Title as="h2">{title}</Title>
+            <FlexContainer gap="20px">
+              <TextAndIcon
+                src="/icons/promotions-section/location.svg"
+                text={location}
+                iconSize="36px"
+                iconSizeMobile="30px"
+                fontSize="21px"
+                fontSizeMobile="18px"
+              />
+              <TextAndIcon
+                src="/icons/cash.svg"
+                text={price}
+                iconSize="36px"
+                iconSizeMobile="30px"
+                fontSize="21px"
+                fontSizeMobile="18px"
+              />
+              <TextAndIcon
+                src="/icons/time.svg"
+                text={date}
+                iconSize="36px"
+                iconSizeMobile="30px"
+                fontSize="21px"
+                fontSizeMobile="18px"
+              />
+            </FlexContainer>
+          </InfoWrap>
+        </TopWrap>
+        <Description>{description}</Description>
+        <SellerInfo
+          sellerName={sellerName}
+          email={email}
+          mobile={mobile}
+          personalCardLink={personalCardLink}
+        />
+        <Description>
+          <b>My preferred contact method:</b> <br />
+          {contactMethod}
+        </Description>
+        <Button
+          text="Back"
+          type="reset"
+          backgroundColor="white"
+          color="blue"
+          onClick={onClose}
+        />
+      </Content>
+    </Modal>
+  );
+};
+
+export default FullAdd;
+
+const Content = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  gap: "24px",
+  alignItems: "flex-start",
+  width: "100%",
+
+  button: {
+    alignSelf: "flex-end",
+  },
+
+  [theme.breakpoints.mobile]: {},
+}));
+
+const TopWrap = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  gap: "24px",
+  alignItems: "center",
+  justifyContent: "space-between",
+  width: "100%",
+
+  [theme.breakpoints.mobile]: {
+    flexDirection: "column",
+  },
+}));
+
+const ImgWrapper = styled("div")(({ theme }) => ({
+  position: "relative",
+  width: "100%",
+  height: "340px",
+  borderRadius: "16px",
+  overflow: "hidden",
+  img: {
+    objectFit: "cover",
+  },
+  border: `2px solid ${theme.colors.yellow}`,
+
+  [theme.breakpoints.mobile]: {
+    minHeight: "200px",
+    width: "100%",
+  },
+}));
+
+const InfoWrap = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  gap: "60px",
+  alignItems: "flex-start",
+
+  [theme.breakpoints.mobile]: {
+    gap: "16px",
+    alignSelf: "flex-start",
+  },
+}));
+
+const Description = styled("span")(({ theme }) => ({
+  fontSize: theme.fontSize.fontS21,
+  fontWeight: 400,
+  lineHeight: "32px",
+  [theme.breakpoints.mobile]: {},
+}));
---
+++ components/sections/agents/delivery/children/seller-info.tsx
@@ -0,0 +1,166 @@
+import React from "react";
+import Image from "next/image";
+import styled from "@emotion/styled";
+import { Title } from "../../../../layout/title";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Link from "next/link";
+
+interface PropsContactRow {
+  email: string;
+  mobile: string;
+}
+interface Props extends PropsContactRow {
+  sellerName: string;
+  personalCardLink?: string | null;
+}
+
+export const SellerInfo = ({
+  sellerName,
+  email,
+  mobile,
+  personalCardLink,
+}: Props) => {
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(email);
+      toast.success("Email copied to clipboard!");
+    } catch (err) {
+      toast.error("Failed to copy email.");
+    }
+  };
+
+  return (
+    <Wrapper>
+      <InfoWrap>
+        <AvatarWrap size={100}>
+          <Image
+            width={50}
+            height={50}
+            src="/icons/agents/human.svg"
+            alt="human icon"
+          />
+        </AvatarWrap>
+        <TextWrap>
+          {personalCardLink ? (
+            <Link href={personalCardLink}>
+              <TitleStyled isLink as="h3">
+                {sellerName}
+              </TitleStyled>
+            </Link>
+          ) : (
+            <TitleStyled as="h3">{sellerName}</TitleStyled>
+          )}
+          <Email onClick={handleCopy}>{email}</Email>
+        </TextWrap>
+      </InfoWrap>
+
+      <ContactRow mobile={mobile} email={email} />
+      <ToastContainer />
+    </Wrapper>
+  );
+};
+
+export const ContactRow = ({ mobile, email }: PropsContactRow) => {
+  return (
+    <ContactRowWrap>
+      <IconCircle href={`tel:${mobile}`} aria-label={`Call ${mobile}`}>
+        <Image
+          width={24}
+          height={24}
+          src="/icons/agents/phone.svg"
+          alt="phone icon"
+        />
+      </IconCircle>
+      <IconCircle href={`mailto:${email}`} aria-label={`Email to ${email}`}>
+        <Image
+          width={24}
+          height={24}
+          src="/icons/agents/mail.svg"
+          alt="mail icon"
+        />
+      </IconCircle>
+    </ContactRowWrap>
+  );
+};
+
+const Wrapper = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  gap: "20px",
+  alignItems: "center",
+  width: "100%",
+
+  [theme.breakpoints.mobile]: {
+    flexDirection: "column",
+    alignItems: "flex-end",
+    gap: "4px",
+  },
+}));
+
+const ContactRowWrap = styled("div")(({ theme }) => ({
+  zIndex: 2,
+  marginTop: "auto",
+  display: "flex",
+  flexDirection: "row",
+  gap: "20px",
+  [theme.breakpoints.mobile]: {
+    justifyContent: "flex-end",
+  },
+}));
+
+const IconCircle = styled("a")(({ theme }) => ({
+  height: 40,
+  width: 40,
+  borderRadius: "50%",
+  alignSelf: "flex-end",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: theme.colors.yellow,
+}));
+
+export const AvatarWrap = styled("div")<{ size?: number }>(
+  ({ theme, size = 50 }) => ({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: size,
+    height: size,
+    minWidth: size,
+    minHeight: size,
+    borderRadius: "50%",
+    backgroundColor: "rgba(255, 185, 1, 0.08)",
+    border: "1px solid",
+    borderColor: theme.colors.yellow,
+  }),
+);
+
+const InfoWrap = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  width: "100%",
+  alignItems: "center",
+  gap: 16,
+
+  [theme.breakpoints.mobile]: {},
+}));
+
+const TextWrap = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  gap: 12,
+  [theme.breakpoints.mobile]: {},
+}));
+
+const TitleStyled = styled(Title)<{ isLink?: boolean }>(
+  ({ theme, isLink }) => ({
+    fontSize: theme.fontSize.fontS32,
+    textDecoration: isLink ? "underline" : "none",
+    [theme.breakpoints.mobile]: {},
+  }),
+);
+const Email = styled("div")(({ theme }) => ({
+  fontSize: theme.fontSize.fontS16 || 16,
+  color: theme.colors.grey3,
+}));
