054807684bb015cc241fe95313463afd0120a25d
okuruchimas <lianeso4kas@gmail.com>
2025-06-18 15:02:33 +0200

    added 500 page

---
+++ pages/500.tsx
@@ -0,0 +1,55 @@
+import { useTranslation } from "next-i18next";
+// components
+import Image from "../components/layout/image";
+// utils
+import styled from "@emotion/styled";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { getLayoutData } from "../utils/get-layout-data";
+
+const ErrorPage = () => {
+  const { t } = useTranslation("common");
+
+  return (
+    <Wrapper>
+      <Image
+        width="300px"
+        height="120px"
+        alt="placeholder image"
+        src="/images/background/background-prom.svg"
+        priority
+      />
+      <h1>{t("500")}</h1>
+    </Wrapper>
+  );
+};
+export default ErrorPage;
+
+export async function getStaticProps({ locale }: any) {
+  const { headerData, footerData } = await getLayoutData(locale);
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, [
+        "common",
+        "entertainers-tour-guides",
+      ])),
+      headerData,
+      footerData,
+    },
+  };
+}
+
+const Wrapper = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  gap: "20px",
+  alignItems: "center",
+  justifyContent: "center",
+
+  height: "calc(100vh - 55px)",
+
+  h1: {
+    color: theme.colors.blue,
+    textAlign: "center",
+  },
+}));
